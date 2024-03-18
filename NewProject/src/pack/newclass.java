770b5ea966c1b46c6a4402307c3d1a2c0beaadb6
tecra <tecra@DESKTOP-L713QJE.alphionsee.in>
2024-03-18 23:24:55 +0530

    new line 2

--- NewProject/src/pack/newclass.java
+++ NewProject/src/pack/newclass.java
@@ -8,7 +8,8 @@ public class newclass {
 		int i=10;
 		System.out.println(i);
 		//System.out.println("Welcome");
-		system.out.println("Git welcome");
+		System.out.println("Git welcome");
+		System.out.println("Eclipse welcome");
 	}
 
 }
